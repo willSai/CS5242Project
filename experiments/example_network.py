c118c07ad19b798761da553ef2605e370d038794
HAO <willlifeyo@gmail.com>
2018-10-12 13:35:10 +0800

    *) edit the draft network model [experiments/example_network.py]

--- experiments/example_network.py
+++ experiments/example_network.py
@@ -2,31 +2,40 @@ import keras
 from keras.models import Model
 from keras.layers import Input, Dense, Conv2D, Activation, MaxPool2D, Dropout, Flatten
 
-def example_network(input_shape=(28,28,1), class_num=10):
+def example_network(input_shape=(21,21,21,19), class_num=10):
     """Example CNN
-    
-    Keyword Arguments:
-        input_shape {tuple} -- shape of input images. Should be (28,28,1) for MNIST and (32,32,3) for CIFAR (default: {(28,28,1)})
-        class_num {int} -- number of classes. Shoule be 10 for both MNIST and CIFAR10 (default: {10})
-    
-    Returns:
-        model -- keras.models.Model() object
+    Mind the 19 in input_shape, it refers to the number of our own features.
     """
 
     im_input = Input(shape=input_shape)
     
-    t = Conv2D(16, (3,3))(im_input)
+    t = Conv3D(64, (11,11,11))(im_input)
     t = Activation('relu')(t)
-    t = MaxPool2D(pool_size=(2,2))(t)
-    t = Dropout(0.9)(t)
+    t = MaxPool3D(pool_size=(3,3,3))(t)
+
+    t = Conv3D(128, (6,6,6))(im_input)
+    t = Activation('relu')(t)
+    t = MaxPool3D(pool_size=(4,4,4))(t)
+
+    t = Conv3D(256, (3,3,3))(im_input)
+    t = Activation('relu')(t)
+    t = MaxPool3D(pool_size=(5,5,5))(t)
 
     t = Flatten()(t)
     
-    t = Dense(256)(t)
+    t = Dense(1000)(t)
     t = Activation('relu')(t)
-    t = Dense(class_num)(t)
+    t = Dropout(0.5)(t)
 
-    output = Activation('softmax')(t)
+    t = Dense(500)(t)
+    t = Activation('relu')(t)
+    t = Dropout(0.5)(t)
+
+    t = Dense(200)(t)
+    t = Activation('relu')(t)
+    t = Dropout(0.5)(t)
+
+    # output = Activation('softmax')(t)
 
     model = Model(input=im_input, output=output)
     
